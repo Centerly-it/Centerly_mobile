12b82d96dc3734c70d237d8a97d56b5f0efb660b
Ahmed Hisham <ahmedhishamrasmy1@gmail.com>
2026-04-17 23:00:03 +0200

    feat: implement SecureStorageHelper class for secure data management

--- lib/core/utils/secure_storage_helper.dart
+++ lib/core/utils/secure_storage_helper.dart
@@ -1,1 +1,29 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
+class SecureStorageHelper {
+  final FlutterSecureStorage _storage;
+
+  SecureStorageHelper() : _storage = const FlutterSecureStorage();
+
+  Future<void> saveData({required String key, required String value}) async {
+    await _storage.write(key: key, value: value);
+  }
+
+  Future<String?> readData({required String key}) async {
+    return await _storage.read(key: key);
+  }
+
+  Future<void> deleteData({required String key}) async {
+    await _storage.delete(key: key);
+  }
+
+  Future<void> clearAll() async {
+    await _storage.deleteAll();
+  }
+}
+
+class StorageKeys {
+  static const String userToken = 'USER_TOKEN';
+  static const String userId = 'USER_ID';
+  static const String languageCode = 'LANGUAGE_CODE';
+}
