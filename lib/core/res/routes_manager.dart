71a56e4c88bb6904946096baec938250f9084043
Ahmed Hisham <ahmedhishamrasmy1@gmail.com>
2026-04-17 14:37:16 +0200

    feat: add dependency injection setup, caching service, and various utility classes

---
+++ lib/core/res/routes_manager.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class RoutesManager {
+  static const String splashScreen = '/';
+}
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+      builder: (_) => Scaffold(
+        appBar: AppBar(title: const Text("No Route Found")),
+        body: const Center(child: Text("No Route Found")),
+      ),
+    );
+  }
+}
