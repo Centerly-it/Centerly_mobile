ac0d5476c7d814f3ab299e108d6db2e87e34d6a5
Ahmed Tamer <a.tamer7112002@gmail.com>
2026-04-27 16:33:04 +0300

    feat: establish foundational architecture and launch experience

--- lib/core/res/assets_manager.dart
+++ lib/core/res/assets_manager.dart
@@ -1,1 +1,7 @@
-class AssetsManager {}
+class AssetsManager {
+  static const String _imagesPath = 'assets/images';
+
+  static const String splashLogo = '$_imagesPath/centrally_splash_Icon.png';
+  static const String centerlyLogo = '$_imagesPath/centerly_logo.png';
+  static const String appIcon = '$_imagesPath/icon.png';
+}
