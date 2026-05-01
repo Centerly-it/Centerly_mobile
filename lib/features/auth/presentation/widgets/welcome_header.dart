c5cfb592944e899bda9128421ab3486f2a118848
24shrouk <shroukhegab3@gmail.com>
2026-05-02 00:44:08 +0300

    refactor(auth): update direction of column & text responsive sizedbox

--- lib/features/auth/presentation/widgets/welcome_header.dart
+++ lib/features/auth/presentation/widgets/welcome_header.dart
@@ -2,10 +2,10 @@
 // Welcome header
 // ---------------------------------------------------------------------------
 
+import 'package:centrally/core/func/extensions.dart';
 import 'package:centrally/core/res/color_manager.dart';
 import 'package:centrally/core/res/strings_manager.dart';
 import 'package:centrally/core/res/style_manager.dart';
-import 'package:centrally/core/res/values_manager.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
@@ -15,16 +15,16 @@ class WelcomeHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
           StringsManager.loginWelcomeTitle.tr(),
           style: AppTextStyles.headlineMedium,
         ),
-        const SizedBox(height: AppSize.s6),
+        SizedBox(height: context.screenHeight * 0.008), // AppSize.s6
         Text(
           StringsManager.loginWelcomeSubtitle.tr(),
-          textAlign: TextAlign.right,
+          textAlign: TextAlign.start,
           style: AppTextStyles.bodyMedium.copyWith(
             color: ColorManager.textSecondary,
           ),
